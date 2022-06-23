d2194599ba5679ee950d58f16095849c4bbe5150
baiziyu-fe <baiziyu-fe@outlook.com>
2022-06-23 12:19:26 +0800

    feat(static-server): static server

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const Koa = require("koa");
-const Json = require('koa-json')
+const StaticServer = require('koa-static');
+const path = require('path');
+const Json = require('koa-json');
 const Logger = require("./middleware/logger");
 const Proxy = require("./middleware/proxy");
 
@@ -9,6 +11,13 @@ const app = new Koa();
 // TODO: MiddleWare
 app.use(Logger);
 app.use(Json())
+
+//TODO: Static Assets
+app.use(StaticServer(path.join(__dirname, '/static')), {
+    dynamic: true
+}, {
+    maxAge: 365 * 24 * 60 * 60
+});
 
 // TODO: Router
 app.use(require('./router/index').routes())
